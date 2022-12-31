4b60ba2e6188b836a24b60dd9e86d4f59841dfe6
konglong <1016361268@qq.com>
2022-12-31 22:17:57 +0800

    字节输出流写出数据的细节

--- src/com/izumi/mybytestream/ByteStreamDemo1.java
+++ src/com/izumi/mybytestream/ByteStreamDemo1.java
@@ -6,8 +6,24 @@ import java.io.IOException;
 //需求：写出一段文字到本地文件中。（暂时不写中文）
 public class ByteStreamDemo1 {
     public static void main(String[] args) throws IOException {
+        /*
+            字节输出流的细节：
+                1. 创建字节输出流对象
+                    细节1：参数是字符串表示的路径或者是File对象都是可以的
+                    细节2：如果文件不存在会创建一个新的文件，但是要保证父级路径是存在的。
+                    细节3：如果文件已经存在，则会清空文件
+
+                2. 写数据
+                    细节：write方法的参数是整数，但是实际上写到本地文件中的是整数在ASCII上对应的字符
+                    97 --> a
+                    100 --> d
+
+                3. 释放资源
+                    每次使用完流之后都要释放资源，不然会一直占用这个文件
+         */
+
         // 1. 创建对象
-        FileOutputStream fos = new FileOutputStream("./a.txt");
+        FileOutputStream fos = new FileOutputStream("./b.txt");
         // 2. 写出数据
         fos.write(97);
         // 3. 释放资源
